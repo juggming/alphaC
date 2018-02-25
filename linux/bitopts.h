5c7077afeb65f7186a35c95eb1c0c5af37699059
juggming <stu_xushangming@163.com>
2018-02-25 19:30:19 +0800

    add some basic bit and roundup operation

---
+++ linux/bitopts.h
@@ -0,0 +1,125 @@
+#ifndef LINUX_BIT_OPTS
+#define LINUX_BIT_OPTS
+
+#include <stdbool.h>
+
+#define BITS_PER_LONG   64
+
+
+/**
+ * fls - find last (most-significant) bit set
+ * @x: the word to search
+ *
+ * This is defined the same way as ffs.
+ * Note fls(0) = 0, fls(1) = 1, fls(0x80000000) = 32.
+ */
+
+
+static inline int fls(int x)
+{
+	int r = 32;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff0000u)) {
+		x <<= 16;
+		r -= 16;
+	}
+	if (!(x & 0xff000000u)) {
+		x <<= 8;
+		r -= 8;
+	}
+	if (!(x & 0xf0000000u)) {
+		x <<= 4;
+		r -= 4;
+	}
+	if (!(x & 0xc0000000u)) {
+		x <<= 2;
+		r -= 2;
+	}
+	if (!(x & 0x80000000u)) {
+		x <<= 1;
+		r -= 1;
+	}
+	return r;
+}
+
+static unsigned long __fls(unsigned long word)
+{
+	int num = BITS_PER_LONG - 1;
+
+#if BITS_PER_LONG == 64
+	if (!(word & (~0ul << 32))) {
+		num -= 32;
+		word <<= 32;
+	}
+#endif
+	if (!(word & (~0ul << (BITS_PER_LONG-16)))) {
+		num -= 16;
+		word <<= 16;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-8)))) {
+		num -= 8;
+		word <<= 8;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-4)))) {
+		num -= 4;
+		word <<= 4;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-2)))) {
+		num -= 2;
+		word <<= 2;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-1))))
+		num -= 1;
+	return num;
+}
+
+/**
+ * fls64 - find last set bit in a 64-bit word
+ * @x: the word to search
+ *
+ * This is defined in a similar way as the libc and compiler builtin
+ * ffsll, but returns the position of the most significant set bit.
+ *
+ * fls64(value) returns 0 if value is 0 or the position of the last
+ * set bit if value is nonzero. The last (most significant) bit is
+ * at position 64.
+ */
+static inline int fls64(unsigned long x)
+{
+	if (x == 0)
+		return 0;
+	return __fls(x) + 1;
+}
+
+static inline unsigned long fls_long(unsigned long l)
+{
+    if(sizeof(l) == 4)
+        return fls(l);
+    return fls64(l);
+}
+static inline bool is_power_of_2(unsigned long n)
+{
+    return (n != 0 && ((n & (n -1)) == 0));
+}
+
+static inline unsigned long roundup_pow_of_two(unsigned long n)
+{
+    return 1UL << fls_long(n -1);
+}
+
+static inline unsigned long rounddown_pow_of_two(unsigned long n)
+{
+    return 1UL << (fls_long(n) -1);
+}
+
+
+/**
+ * __fls - find last (most-significant) set bit in a long word
+ * @word: the word to search
+ *
+ * Undefined if no set bit exists, so code should check against 0 first.
+ */
+
+#endif /* _ASM_GENERIC_BITOPS___FLS_H_ */
